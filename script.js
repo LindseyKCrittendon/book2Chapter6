bd69eea5bae372b8a7705bf6484499413505d0cf
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-15 15:04:59 -0400

    starting practice exercise

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 
-//doer function, not a maker function
-function applyGoatMode(){
-    //Select body element
-    const bodyElement = document.querySelector("body"); //will select one element at a time
-    console.log("this should be the body element",bodyElement);
-    //apply class of .goat-mode to the body element.
-    bodyElement.classList.add("goat-mode");
+// //doer function, not a maker function
+// function applyGoatMode(){
+//     //Select body element
+//     const bodyElement = document.querySelector("body"); //will select one element at a time
+//     console.log("this should be the body element",bodyElement);
+//     //apply class of .goat-mode to the body element.
+//     bodyElement.classList.add("goat-mode");
 
-    //select all of list items and turn their background colors orange
-    const listItems = document.querySelectorAll(".list-item");
-    console.log(listItems)
-    listItems.classList.add("orange-background");
-}
-applyGoatMode();
+//     //select all of list items and turn their background colors orange
+//     const listItems = document.querySelectorAll(".list-item");
+//     console.log(listItems)
+//     listItems.classList.add("orange-background");
+// }
+// applyGoatMode();
+
+// Practice
+// Add the following code to a new HTML file. Make sure you include a stylesheet named blog.css.
+
+// Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
+
+
+// Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
+
+
+// Obtain a reference the element with a class of dashed and remove it.
+
+
+// Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
