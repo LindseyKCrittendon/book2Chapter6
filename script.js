0521c8fad2e2ea3c6fb7fe201c52e61caa076dd2
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-15 15:47:13 -0400

    first part of exercise done two different ways.

--- script.js
+++ script.js
@@ -20,6 +20,18 @@
 // Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
 
 
+// let pageTitle = document.querySelector(".article__header").textContent;
+// pageTitle = "Welcome to the Lindsey blog";
+// document.querySelector(".article__header").textContent = pageTitle;
+// console.log(pageTitle);
+
+function changeText(){
+    let articleHeader = document.querySelector('.article__header');
+    articleHeader.textContent = "Welcome to Lindsey's blog";
+}
+changeText()
+
+
 // Use JavaScript to obtain a reference to all article__header elements and change their classList property value to article__header important.
 
 
