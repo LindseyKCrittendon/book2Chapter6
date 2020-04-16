8660d9337037d306e1526cdf0ef78ffddf0274fe
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-16 09:29:21 -0400

    after review

--- script.js
+++ script.js
@@ -25,12 +25,14 @@
 // Add the following code to a new HTML file. Make sure you include a stylesheet named blog.css.
 
 // Use JavaScript to obtain a reference to the first section with the class of article_header and change its text with the textContent property to "Welcome to the {insert your name here} blog"
-
-
+const headerElement = document.querySelector(".article__header")
+console.log(headerElement)
 // let pageTitle = document.querySelector(".article__header").textContent;
 // pageTitle = "Welcome to the Lindsey blog";
 // document.querySelector(".article__header").textContent = pageTitle;
 // console.log(pageTitle);
+//below is the simplest way
+// headerElement.textContent = "Welcome to the Lindsey blogathon"
 
 function changeText(){
     let articleHeader = document.querySelector('.article__header');
@@ -46,7 +48,15 @@ for(let i = 0; i < everyHeader.length; i++){
 everyHeader[i].classList.add("important");
 }
 console.log(everyHeader);
+//review answers
+// const allHeaderElements = document.querySelectorAll("article__header")
+// console.log("this is all of them", allHeaderElements)
+// for(let i = 0; i < allHeaderElements.length; i++){
+// allHeaderElements[i].classList.add("important")
+// }
 // Obtain a reference the element with a class of dashed and remove it.
-
+document.querySelector(".dashed").classList.remove("dashed")
 
 // Obtain a reference the element with a class of article_footer and add the class of goldenrod it.
+
+document.querySelector(".article__footer").classList.add("goldenrod")
